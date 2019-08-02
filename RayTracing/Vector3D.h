74f57d84840a3bfeaca817ec7fc39412102361fb
terraritto <28126083+terraritto@users.noreply.github.com>
2019-08-03 00:22:51 +0900

    add Vector and Point

---
+++ RayTracing/Vector3D.h
@@ -0,0 +1,32 @@
+#pragma once
+
+class Vector3D
+{
+public:
+	double mPosX, mPosY, mPosZ;
+
+	Vector3D();
+	Vector3D(double a);
+	Vector3D(double x, double y, double z);
+	Vector3D(const Vector3D& v);
+	Vector3D(const class Normal& n);
+	Vector3D(const class Point3D& p);
+	~Vector3D();
+	Vector3D& operator=(const Vector3D& rhs);
+	Vector3D& operator=(const class Normal& rhs);
+	Vector3D& operator=(const class Point3D& rhs);
+
+	Vector3D operator-() const;
+	Vector3D operator*(const double a) const;
+	Vector3D operator/(const double a) const;
+	Vector3D operator+(const Vector3D& v) const;
+	Vector3D operator+=(const Vector3D& v);
+	Vector3D operator-(const Vector3D& v) const;
+	double operator*(const Vector3D& b) const;
+	Vector3D operator^(const Vector3D& v) const;
+
+	double LengthSq();
+	double Length();
+	void Normalize();
+	Vector3D& hat();
+};
