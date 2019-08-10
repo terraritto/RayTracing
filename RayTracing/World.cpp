728ed86b8db652ad012384ff4b5aa540a7c4f7ac
terraritto <28126083+terraritto@users.noreply.github.com>
2019-08-10 20:01:06 +0900

    prepare function for bare-bone ray tracer.

---
+++ RayTracing/World.cpp
@@ -0,0 +1,9 @@
+#include "Constants.h"
+#include "Vector3D.h"
+#include "Point3D.h"
+#include "Normal.h"
+#include "Ray.h"
+#include "World.h"
+
+//build is separated
+#include "BuildRedSphere.cpp"
--- RayTracing/World.h
+++ RayTracing/World.h
@@ -1,6 +1,23 @@
 #pragma once
+#include "ViewPlane.h"
+#include "RGBColor.h"
+#include "Sphere.h"
+#include "Tracer.h"
+#include <memory>
+
 
 class World
 {
+public:
+	ViewPlane mViewPlane;
+	RGBColor mBackGroundColor;
+	Sphere mSphere;
+	std::shared_ptr<Tracer> mTracerPtr;
 
+	World();
+	
+	void Build();
+	void RenderScene() const;
+	void OpenWindow(const int hres, const int vres) const;
+	void DisplayPixel(const int row, const int column, const RGBColor& pixelColor) const;
 };
