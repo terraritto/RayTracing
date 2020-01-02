7ef43c2580a91e9510dbc5b4aa3a63cc37939061
terraritto <28126083+terraritto@users.noreply.github.com>
2020-01-03 03:09:40 +0900

    add Mesh Triangle.

---
+++ RayTracing/Light/Objects/MeshTriangle.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "GeometricObject.h"
+#include "Mesh.h"
+
+class MeshTriangle :public GeometricObject
+{
+	
+public:
+	std::shared_ptr<Mesh> mMeshPtr;
+	int mIndex0, mIndex1, mIndex2;
+	Normal mNormal;
+	float mArea;
+public:
+	MeshTriangle();
+	MeshTriangle(std::shared_ptr<Mesh> mesh_ptr, const int i1, const int i2, const int i3);
+	MeshTriangle(const MeshTriangle& mt);
+	virtual ~MeshTriangle();
+	MeshTriangle& operator=(const MeshTriangle& rhs);
+
+	std::shared_ptr<MeshTriangle> Clone() const;
+
+	virtual bool hit(const Ray& ray, double& tMin, ShadeRec& sr) override;
+	bool Shadow_hit(const Ray& ray, float& tmin) const override final;
+
+	void ComputeNormal(const bool reverse_normal);
+
+	virtual Normal GetNormal(const Point3D& p) override;
+	virtual BBox GetBoundingBox() const override;
+
+};
