1937642ac1c9d8e2b6a41a14ba8fd83b7a1f48f0
terraritto <28126083+terraritto@users.noreply.github.com>
2020-01-28 00:13:36 +0900

    add Lambertian for texture.

---
+++ RayTracing/Light/BRDF/SV_Lambertian.cpp
@@ -0,0 +1,103 @@
+#include "SV_Lambertian.h"
+#include "..//Maths/Constants.h"
+#include "../Sampler/MultiJittered.h"
+
+SV_Lambertian::SV_Lambertian()
+	: BRDF()
+	, mKd(0.0)
+	, mCd()
+{
+
+}
+
+SV_Lambertian::SV_Lambertian(const SV_Lambertian& lamb)
+	: BRDF(lamb)
+	, mKd(lamb.mKd)
+	, mCd(lamb.mCd)
+{
+	mSamplerPtr->MapSamplesToHemisphere(1);
+}
+
+SV_Lambertian& SV_Lambertian::operator=(const SV_Lambertian& rhs)
+{
+	if (this == &rhs)
+	{
+		return *this;
+	}
+
+	BRDF::operator=(rhs);
+
+	mKd = rhs.mKd;
+	if (rhs.mCd)
+	{
+		mCd = rhs.mCd;
+	}
+	else
+	{
+		mCd = std::make_shared<Texture>();
+	}
+
+	return *this;
+}
+
+SV_Lambertian::~SV_Lambertian()
+{
+
+}
+
+std::shared_ptr<SV_Lambertian> SV_Lambertian::Clone() const
+{
+	return std::make_shared<SV_Lambertian>(*this);
+}
+
+RGBColor SV_Lambertian::Func(const ShadeRec& sr, const Vector3D& wo, const Vector3D& wi) const
+{
+	return mKd * mCd->GetColor(sr) * INVPI;
+}
+
+RGBColor SV_Lambertian::SampleFunc(const ShadeRec& sr, const Vector3D& wo, Vector3D& wi, float& pdf) const
+{
+	Vector3D w = sr.mNormal;
+	Vector3D v = Vector3D(0.0034, 1.0, 0.0071) ^ w;
+	v.Normalize();
+	Vector3D u = v ^ w;
+
+	Point3D sp = mSamplerPtr->SampleHemisphere();
+	wi = sp.mPosX * u + sp.mPosY * v + sp.mPosZ * w;
+	wi.Normalize();
+	pdf = sr.mNormal * wi * INVPI;
+
+	return mKd * mCd->GetColor(sr) * INVPI;
+}
+
+RGBColor SV_Lambertian::Rho(const ShadeRec& sr, const Vector3D& wo) const
+{
+	return mKd * mCd->GetColor(sr);
+}
+
+void SV_Lambertian::SetKa(const float ka)
+{
+	mKd = ka;
+}
+
+void SV_Lambertian::SetKd(const float kd)
+{
+	mKd = kd;
+}
+
+void SV_Lambertian::SetCd(std::shared_ptr<Texture> cd)
+{
+	mCd = cd;
+}
+
+void SV_Lambertian::SetSampler(std::shared_ptr<Sampler> sp)
+{
+	mSamplerPtr = sp;
+	mSamplerPtr->MapSamplesToHemisphere(1);
+}
+
+void SV_Lambertian::SetSamples(const int numSamples)
+{
+	mSamplerPtr = std::make_shared<MultiJittered>(numSamples);
+	mSamplerPtr->MapSamplesToHemisphere(1);
+}
